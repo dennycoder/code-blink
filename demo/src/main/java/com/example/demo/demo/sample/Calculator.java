b9ffb13ea5135ce6fb283d400a8217be91051a87
dennycoder <154223177+dennycoder@users.noreply.github.com>
2026-09-01 01:13:12 +0530

    Update Calculator.java

--- demo/src/main/java/com/example/demo/demo/sample/Calculator.java
+++ demo/src/main/java/com/example/demo/demo/sample/Calculator.java
@@ -19,12 +19,9 @@ public class Calculator {
     // aaded the new change
     // this the second chnage
     // this is the third change
-<<<<<<< HEAD
 
     // this is the fifth change
-=======
     // this is the fourth change
->>>>>>> 369bc1730fe154c3ee161d390e9b8f5b412a6dad
 /////// this is my first file and i want to comit the chanbes fsdf
     public double divide(double a, double b) {
         if (b == 0) {
